6081a57fa88587f0e19d1de2b2dab25aad793d4b
Aya Hosny <ayazaine@gmail.com>
2022-09-10 00:51:11 +0200

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,12 +39,12 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label for="user_id" class="col-md-4 col-form-label text-md-end">{{ __('User name') }}</label>
+                            <label for="user_name" class="col-md-4 col-form-label text-md-end">{{ __('User name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="user_id" type="text" class="form-control @error('user_id') is-invalid @enderror" name="user_id" value="{{ old('user_id') }}" required autocomplete="user_id">
+                                <input id="user_name" type="text" class="form-control @error('user_name') is-invalid @enderror" name="user_name" value="{{ old('user_name') }}" required autocomplete="user_name">
 
-                                @error('user_id')
+                                @error('user_name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
