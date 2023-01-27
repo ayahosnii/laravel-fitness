c34dd72ce7b8a5b2acfedbeacb16dfeba01f412c
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2023-01-27 21:18:02 +0200

    blog

---
+++ app/Http/Controllers/BlogCategoyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BlogCategoy;
+use Illuminate\Http\Request;
+
+class BlogCategoyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BlogCategoy  $blogCategoy
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BlogCategoy $blogCategoy)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BlogCategoy  $blogCategoy
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BlogCategoy $blogCategoy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BlogCategoy  $blogCategoy
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BlogCategoy $blogCategoy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BlogCategoy  $blogCategoy
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BlogCategoy $blogCategoy)
+    {
+        //
+    }
+}
