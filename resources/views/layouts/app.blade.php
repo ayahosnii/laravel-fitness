336dc889d06f113f338e123f3cafcbf2e49b22f2
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2023-04-07 13:28:51 +0200

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -158,8 +158,9 @@
                                 <li class="scroll-to-section"><a href="#our-classes">Classes</a></li>
                                 <li class="scroll-to-section"><a href="#schedule">Schedules</a></li>
                                 <li class="scroll-to-section"><a href="#contact-us">Contact</a></li>
+                                <li class="scroll-to-section"><a href="#">Login</a></li>
+
                                 <li class="main-button"><a href="#">Sign Up</a></li>
-                                <li class="scroll-to-section"><a href="#">Contact</a></li>
                             </ul>
                             <a class='menu-trigger'>
                                 <span>Menu</span>
@@ -178,12 +179,17 @@
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="{{asset('assets/js/home-js/jquery-2.1.0.min.js')}}"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="{{asset('assets/js/home-js/popper.js')}}"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
     <script src="{{asset('assets/js/main.js')}}"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
 
-    <script src="{{asset('assets/js/home-js/jquery-2.1.0.min.js')}}"></script>
     <script type="text/javascript" src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
 
 
@@ -193,7 +199,6 @@
 --}}
 
     <!-- Bootstrap -->
-    <script src="{{asset('assets/js/home-js/popper.js')}}"></script>
     <script src="{{asset('assets/js/home-js/bootstrap.min.js')}}"></script>
 
     <!-- Plugins -->
